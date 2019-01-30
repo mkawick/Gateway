fa07b018c9e92daae06f5463269bfba81201f5c7
Mickey Kawick <mkawick@yahoo.com>
2019-01-30 07:57:20 +0000

    Cleanup of directory

--- Microservices/SpatialPartitioningTest01/Program.cs
+++ Microservices/SpatialPartitioningTest01/Program.cs
@@ -31,6 +31,12 @@ namespace SpatialPartitioningTest01
         {
             spaceObject = new SpaceObject();
         }
+        public Asteroid Duplicate()
+        {
+            Asteroid ast = new Asteroid();
+            ast.spaceObject.position = spaceObject.position;
+            return ast;
+        }
     }
     class Program
     {
@@ -70,6 +76,11 @@ namespace SpatialPartitioningTest01
                 }
 
                 Console.WriteLine("Num successes: {0} out of {1} test runs", successCount, numTestRuns);
+            }
+
+            if(RunDeleteTest1((int) 0, (int)0, 500, ast, partition) == false)
+            {
+                Console.WriteLine("RunDeleteTest1 failed");
             }
            /* List<SpaceObject> myList = partition.GetAll((int)center.x, (int)center.z, dist);
 
@@ -144,30 +155,43 @@ namespace SpatialPartitioningTest01
         {
             partition.ClearAll();
             partition.Add(ast[0].spaceObject);
+            Asteroid astDup = ast[0].Duplicate();
+            partition.Add(astDup.spaceObject);
 
-            Vector3 center = new Vector3(-100, 0, -100);
-            int dist = 1200;
+            Vector3 center = ast[0].spaceObject.position;
+            int dist = 500;
             List<SpaceObject> myList = partition.GetAll((int)center.x, (int)center.z, dist);
 
-        /*    foreach (var t in myList)
+            int countToFind = 2;
+            foreach(var obj in myList)
             {
-                if (Vector3.DistanceSquared(center, t.position) < dist * dist)
+                if(obj == ast[0].spaceObject)
                 {
-                    //   Console.WriteLine("valid vector");
+                    countToFind--;
                 }
-                else
+                else if( obj == astDup.spaceObject)
                 {
-                    //   Console.WriteLine("**** invalid vector ****");
+                    countToFind--;
                 }
             }
 
+            if(countToFind != 0)
+            {
+                Console.WriteLine("RunDeleteTest1:: invalid storage of vectors ****");
+            }
+            partition.Remove(astDup.spaceObject);
+            List<SpaceObject> myList2 = partition.GetAll((int)center.x, (int)center.z, dist);
 
-            int countAsteroids = GetNumAsteroidsClose(ast, (int)center.x, (int)center.z, dist);*/
-            // Console.WriteLine("num close by dist is: {0}", countAsteroids);
-
-
-            if (countAsteroids == myList.Count)
+            if(myList2.Count != myList.Count -1)
+            {
+                Console.WriteLine("RunDeleteTest1:: wrong number of items deleted");
+            }
+            else
+            {
+                Console.WriteLine("RunDeleteTest1:: SUCCESS");
                 return true;
+            }
+
             return false;
         }
     }
@@ -176,296 +200,4 @@ namespace SpatialPartitioningTest01
 
 }
 
-
-namespace SpatialPartitionPattern
-{
-    public class Grid
-    {
-        //Need this to convert from world coordinate position to cell position
-        int cellSize;
-        int rangeMin, rangeMax;
-        int numberOfCells;
-
-        //This is the actual grid, where a SpaceObject is in each cell
-        //Each individual SpaceObject links to other SpaceObjects in the same cell
-        SpaceObject[,] cells;
-        List<SpaceObject> recentlyAddedObjects;
-        List<SpaceObject> recentlyMovedObjects;
-        List<SpaceObject> recentlyDeletedObjects;
-
-        public int RangeMin
-        {
-            get { return rangeMin; }
-        }
-        public int RangeMax
-        {
-            get { return rangeMax; }
-        }
-
-        //Init the grid
-        public Grid(float mapWidth, int cellSize)
-        {
-            this.cellSize = cellSize;
-            this.rangeMin = (int)-mapWidth;
-            this.rangeMax = (int)mapWidth;
-            /*mapWidth += cellSize;// need to allow off-by-one
-            mapWidth *= 2;*/
-
-            numberOfCells = (int)mapWidth / cellSize;
-
-            cells = new SpaceObject[numberOfCells, numberOfCells];
-            recentlyAddedObjects = new List<SpaceObject>();
-            recentlyMovedObjects = new List<SpaceObject>();
-            recentlyDeletedObjects = new List<SpaceObject>();
-        }
-
-        public void ClearAll()
-        {
-            for(int z=0; z< numberOfCells; z++ )
-            {
-                for(int x=0; x< numberOfCells; x++)
-                {
-                    cells[x, z] = null;
-                }
-            }
-            recentlyAddedObjects.Clear();
-            recentlyMovedObjects.Clear();
-            recentlyDeletedObjects.Clear();
-        }
-        
-        public void Add(SpaceObject spaceObject)
-        {
-            //Determine which grid cell the SpaceObject is in
-            int cellX = NormalizeCell(spaceObject.position.x);
-            int cellZ = NormalizeCell(spaceObject.position.z);
-
-            //Add the soldier to the front of the list for the cell it's in
-            spaceObject.prev = null;
-            spaceObject.next = cells[cellX, cellZ];
-            
-            cells[cellX, cellZ] = spaceObject;
-
-            if (spaceObject.next != null)
-            {
-                //Set this soldier to be the previous soldier of the next soldier of this soldier (linked lists ftw)
-                spaceObject.next.prev = spaceObject;
-            }
-            recentlyAddedObjects.Add(spaceObject);
-        }
-
-        public void Remove(SpaceObject spaceObject)
-        {
-            //Determine which grid cell the SpaceObject is in
-            int cellX = NormalizeCell(spaceObject.position.x);
-            int cellZ = NormalizeCell(spaceObject.position.z);
-
-            SpaceObject temp = cells[cellX, cellZ];
-
-            while (temp != null)
-            {
-                if(temp == spaceObject)
-                {
-                    recentlyDeletedObjects.Add(spaceObject);
-                    if (temp.prev != null)
-                    {
-                        temp.prev.next = temp.next;
-                        if (temp.next != null)
-                        {
-                            temp.next.prev = temp.prev;
-                        }
-                    }
-                    else
-                    {
-                        cells[cellX, cellZ] = temp.next;
-                        temp.next.prev = null;
-                    }
-                    break;
-                }
-                temp = temp.next;
-            }           
-        }
-
-        int NormalizeCell(float pos)
-        {
-            int floor = ((int)pos - rangeMin);
-            //floor += Math.Sign(floor) * (cellSize >> 1);// rounding off 
-            int cell = floor / cellSize;
-            return cell;
-        }
-        //Get the closest enemy from the grid
-        public SpaceObject FindClosestEnemy(SpaceObject spaceObject)
-        {
-            //Determine which grid cell the friendly soldier is in
-            int cellX = NormalizeCell(spaceObject.position.x);
-            int cellZ = NormalizeCell(spaceObject.position.z);
-
-            //Get the first enemy in grid
-            SpaceObject enemy = cells[cellX, cellZ];
-
-            //Find the closest soldier of all in the linked list
-            SpaceObject closestSpaceObject = null;
-
-            float bestDistSqr = (float) double.PositiveInfinity;
-
-            //Loop through the linked list
-            while (enemy != null)
-            {
-                //The distance sqr between the soldier and this enemy
-                float distSqr = Vector3.DistanceSquared(enemy.position, spaceObject.position);
-
-                //If this distance is better than the previous best distance, then we have found an enemy that's closer
-                if (distSqr < bestDistSqr)
-                {
-                    bestDistSqr = distSqr;
-
-                    closestSpaceObject = enemy;
-                }
-
-                //Get the next enemy in the list
-                enemy = enemy.next;
-            }
-
-            return closestSpaceObject;
-        }
-
-
-        //A soldier in the grid has moved, so see if we need to update in which grid the soldier is
-        public void Move(SpaceObject spaceObject, Vector3 oldPos)
-        {
-            //See which cell it was in 
-            int oldCellX = NormalizeCell(oldPos.x);
-            int oldCellZ = NormalizeCell(oldPos.z);
-
-            //See which cell it is in now
-            int cellX = NormalizeCell(spaceObject.position.x);
-            int cellZ = NormalizeCell(spaceObject.position.z);
-
-            //If it didn't change cell, we are done
-            if (oldCellX == cellX && oldCellZ == cellZ)
-            {
-                return;
-            }
-
-            //Unlink it from the list of its old cell
-            if (spaceObject.prev != null)
-            {
-                spaceObject.prev.next = spaceObject.next;
-            }
-
-            if (spaceObject.next != null)
-            {
-                spaceObject.next.prev = spaceObject.prev;
-            }
-
-            //If it's the head of a list, remove it
-            if (cells[oldCellX, oldCellZ] == spaceObject)
-            {
-                cells[oldCellX, oldCellZ] = spaceObject.next;
-            }
-
-            //Add it bacl to the grid at its new cell
-            Add(spaceObject);
-            recentlyMovedObjects.Add(spaceObject);
-        }
-
-        private int AssignAndRangeCheck(int p, int range)
-        {
-            int minP = p + range;
-            if (minP < rangeMin)
-            {
-                minP = rangeMin;
-            }
-            if (minP > rangeMax)
-            {
-                minP = rangeMin;
-            }
-            return minP;
-        }
-
-      /*  private SpaceObject[] GrabAllObjectsInRange(int minX, int maxX, int minZ, int maxZ)
-        {
-            int remainderX = minX % cellSize;
-            minX /= cellSize;
-            if(remainderX>0)
-            {
-                minX--;
-            }
-        }*/
-
-     /*   private SpaceObject requestCell(int posX, int posZ)
-        {
-            int x = posX + cellsToAddForNormalization;
-            int z = posZ + cellsToAddForNormalization;
-
-            return cells[x, z];
-        }*/
-        float GetDistanceSquared(float x1, float z1, float x2, float z2)
-        {
-            float xrange = x2 - x1;
-            float zrange = z2 - z1;
-            float dist = xrange * xrange + zrange * zrange;
-            return dist;
-        }
-
-        public List<SpaceObject> GetAll(int searchX, int searchZ, int range)
-        {
-            int r = Math.Abs(range);
-            int temp = AssignAndRangeCheck(searchX, -r);
-            int minCellX = NormalizeCell(temp);
-            temp = AssignAndRangeCheck(searchX, r);
-            int maxCellX = NormalizeCell(temp);
-            temp = AssignAndRangeCheck(searchZ, -r);
-            int minCellZ = NormalizeCell(temp);
-            temp = AssignAndRangeCheck(searchX, r);
-            int maxCellZ = NormalizeCell(temp);
-
-            List<SpaceObject> objects = new List<SpaceObject>();
-            int distanceSquared = range * range;
-            for(int z= minCellZ; z<= maxCellZ;z++)
-            {
-                for(int x=minCellX; x<= maxCellX; x++)
-                {
-                    SpaceObject obj = cells[x, z];
-                    while(obj != null)
-                    {
-                        if(GetDistanceSquared(obj.position.x, obj.position.z, searchX, searchZ) < distanceSquared)
-                        {
-                            objects.Add(obj);
-                        }
-                        obj = obj.next;
-                    }
-                }
-            }
-            
-            return objects;
-        }
-
-        int GetCount(int cellX, int cellZ)
-        {
-            int count = 0;
-            SpaceObject obj = cells[cellX, cellZ];
-
-            while(obj != null)
-            {
-                count++;
-                obj = obj.next;
-            }
-            return count;
-        }
-        public void PrintCountPerCell()
-        {
-            Console.WriteLine("---------------------------------------------------------");
-            int numAcross = (rangeMax - rangeMin) / cellSize;
-            for (int y = 0; y < numAcross; y++)
-            {
-                for (int x = 0; x < numAcross; x++)
-                {
-                    Console.Write("{0}\t", GetCount(x, y));
-                }
-                Console.Write("\n");
-            }
-            Console.WriteLine("---------------------------------------------------------");
-        }
-    }
-}
 
